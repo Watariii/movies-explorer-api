a3cde27afdc4c878efc26205fcca49879620d599
Nikita Sukhov <veyron-nike@yandex.ru>
2023-07-23 15:22:28 +0300

    limiter вынесена в отдельную мидллвару, logger  запросов вынесен выше limiter

--- app.js
+++ app.js
@@ -5,26 +5,22 @@ const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
 
 const { PORT, DB_URL } = require('./config');
 
 const routes = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlevares/logger');
 const errorHandler = require('./middlevares/error');
-const NotFoundError = require('./error/error-not-found');
+const limiter = require('./middlevares/limiter');
 
-const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-});
+const NotFoundError = require('./error/error-not-found');
 
 const app = express();
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
 });
+
+app.use(requestLogger);
 
 app.use(limiter);
 app.use(helmet());
@@ -33,7 +29,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(requestLogger);
 app.use(routes);
 app.use((req, res, next) => {
   next(new NotFoundError('Ошибка запроса, не найден путь'));
